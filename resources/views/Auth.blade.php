66de2ec36b6be8b70b85ea4b0aeaeee364c840dd
Troia <vadym.queensoft@gmail.com>
2021-08-29 19:17:08 +0300

    Авторизация

--- resources/views/Auth.blade.php
+++ resources/views/Auth.blade.php
@@ -2,7 +2,7 @@
 <html >
    <head>
       <meta charset="UTF-8">
-      <title>Material Compact Login Animation</title>
+      <title>Авторизация</title>
       <link rel='stylesheet prefetch' href='http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'>
       <link rel='stylesheet prefetch' href='http://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900&subset=latin,latin-ext'>
       <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
