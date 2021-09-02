b1a23db222f3ab4415672b15aab9f563aedeee7c
Admirated <vovanebozhinsky@gmail.com>
2021-09-02 10:58:45 +0300

    Добавил карточку, поменял иконки

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -5,7 +5,7 @@
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-   <link rel="stylesheet" href="{{asset('Users/troia/shop/css/style.css')}}">
+   <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
    <script src="https://kit.fontawesome.com/e573f55991.js" crossorigin="anonymous"></script>
    <title>CNSHOP</title>
 </head>
@@ -30,6 +30,6 @@
       </div>
    </main>
 
-   <script src="{{asset('assets/taobao/assets/script.js')}}"></script>
+   <script src="{{asset('assets/js/index.js')}}"></script>
 </body>
 </html>
