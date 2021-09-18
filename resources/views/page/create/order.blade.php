6aacada9210874832dd61a12ca5b79f7b2cd8dd7
Admirated <70275437+Admirated@users.noreply.github.com>
2021-09-18 21:45:34 +0300

    Сделал отправку данных бланка на сервер

--- resources/views/page/create/order.blade.php
+++ resources/views/page/create/order.blade.php
@@ -1,16 +1,16 @@
 <form id="CreateOrder" enctype="multipart/form-data">
    @csrf
    <div class="form-wrapper container ps-0">
-      <div class="card-input d-flex justify-content-between">
+      <div id="0" class="card-input d-flex justify-content-between">
          <div id="photoInputs-0" class="order-card ms-3" style="min-width: 135px;" data-count="0">
             <div class="label-card">
-               <label for="image[]" class="order-title">Фото товара:</label>
+               <label class="order-title">Фото товара:</label>
                <p class="order-subtitle">Загрузить фото товара для заказа</p>
             </div>
             <div id="filesGroup" class="mt-2 row" style="margin-top: 15.4px!important;">
                <p class="numeration">1</p>
                <div class="input-group col-3">
-                  <input id="file-input-0-0" type="file" name="image[]" class="m-0 files-input" required>
+                  <input id="file-input-0-0" type="file" name="0" class="m-0 files-input" onchange="getImageUrl(event)">
                   <div class="file-label-wrapper w-100">
                      <label class="file-label" for="file-input-0-0">Загрузите фото товара</label>
                   </div>
@@ -24,7 +24,7 @@
             </div>
             <div id="UrlPhotoGroup" class="mt-3 row">
                <div>
-                  <input type="text" name="ImageUrl-1" class="m-0 urlPhotos-input text-order-input" placeholder="Вставьте ссылку на фото" required>
+                  <input type="text" name="ImageUrl" class="m-0 urlPhotos-input text-order-input" placeholder="Вставьте ссылку на фото" required>
                </div>
             </div>
          </div>
@@ -35,7 +35,7 @@
             </div>
             <div id="UrlProductGroup" class="mt-3 row">
                <div class="position-relative">
-                  <input type="text" name="url-1" class="m-0 urlProduct-input text-order-input" placeholder="Вставьте ссылку на товар" required>
+                  <input type="text" name="url" class="m-0 urlProduct-input text-order-input" placeholder="Вставьте ссылку на товар" required>
                   <span id="plusGreen-0" class="plus-green-btn"><i class="fas fa-plus"></i></span>
                </div>
             </div>
@@ -58,7 +58,7 @@
             </div>
             <div class="mt-3 row">
                <div>
-                  <input type="text" name="cost" class="m-0 text-order-input" required>
+                  <input type="number" name="cost" class="m-0 text-order-input" required>
                </div>
             </div>
          </div>
@@ -80,7 +80,7 @@
             </div>
             <div class="mt-3 row">
                <div>
-                  <input type="number" name="size" class="m-0 text-order-input" required>
+                  <input type="text" name="size" class="m-0 text-order-input" required>
                </div>
             </div>
          </div>
@@ -101,9 +101,17 @@
          </div>
       </div>
    </div>
+   <div class="mt-4 submitBtn">
+      <button class="btn btn-primary">Создать заказ</button>
+   </div>
 </form>
 
 <script>
+   document.querySelector('#CreateOrder').addEventListener('submit', (e) => {
+      e.preventDefault();
+      makeOrder();
+   })
+
    const greenPlusBtn = document.querySelector('.plus-green-btn');
    let countOfMainInputs = 0;
 
@@ -134,7 +142,7 @@
          <div id="filesGroup" class="mt-2 rowms-2" style="margin-top: 7.8px!important;">
                <p class="subnumeration ms-2" style="${style}">${id + 1}.${count}</p>
                <div class="input-group col-3 p-0">
-                  <input id="file-input-${id}-${count}" type="file" name="image[]" class="m-0 files-input" required>
+                  <input id="file-input-${id}-${count}" type="file" name="${id}" class="m-0 files-input"  onchange="getImageUrl(event)">
                   <div class="file-label-wrapper ms-4">
                      <label class="file-label-mini" for="file-input-${id}-${count}">Загрузите фото товара</label>
                   </div>
@@ -144,14 +152,14 @@
       url.innerHTML = `
          <div id="UrlPhotoGroup" class="row">
             <div>
-               <input type="text" name="ImageUrl-1" class="m-0 urlPhotos-input text-order-input mini-input" placeholder="Вставьте ссылку на фото" required>
+               <input type="text" name="ImageUrl" class="m-0 urlPhotos-input text-order-input mini-input" placeholder="Вставьте ссылку на фото" required>
             </div>
          </div>
       `;
       product.innerHTML = `
          <div id="UrlProductGroup" class="row">
             <div class="position-relative">
-               <input type="text" name="url-1" class="m-0 urlProduct-input text-order-input mini-input" placeholder="Вставьте ссылку на товар" required>
+               <input type="text" name="url" class="m-0 urlProduct-input text-order-input mini-input" placeholder="Вставьте ссылку на товар" required>
             </div>
          </div>
       `;
@@ -178,13 +186,13 @@
       const count = countOfMainInputs;
 
       div.innerHTML = `
-         <div class="card-input d-flex justify-content-between">
+         <div id="${count}" class="card-input d-flex justify-content-between">
             <div id="photoInputs-${count}" class="order-card ms-3" style="min-width: 135px;" data-count="0">
                <div></div>
                <div id="filesGroup" class="mt-2 row" style="margin-top: 7.8px!important;">
                   <p class="numeration">${count + 1}</p>
                   <div class="input-group col-3">
-                     <input id="file-input-${count}-0" type="file" name="image[]" class="m-0 files-input" required>
+                     <input id="file-input-${count}-0" type="file" name="${count}" class="m-0 files-input"  onchange="getImageUrl(event)">
                      <div class="file-label-wrapper w-100">
                         <label class="file-label" for="file-input-${count}-0">Загрузите фото товара</label>
                      </div>
@@ -194,14 +202,14 @@
             <div id="urlPhotoInputs-${count}" class="order-card" style="min-width: 145px;">
                <div id="UrlPhotoGroup" class="mt-2 row">
                   <div>
-                     <input type="text" name="ImageUrl-1" class="m-0 urlPhotos-input text-order-input" placeholder="Вставьте ссылку на фото" required>
+                     <input type="text" name="ImageUrl" class="m-0 urlPhotos-input text-order-input" placeholder="Вставьте ссылку на фото" required>
                   </div>
                </div>
             </div>
             <div id="urlProductInputs-${count}" class="order-card" style="min-width: 150px;">
                <div id="UrlProductGroup" class="mt-2 row">
                   <div class="position-relative">
-                     <input type="text" name="url-1" class="m-0 urlProduct-input text-order-input" placeholder="Вставьте ссылку на товар" required>
+                     <input type="text" name="url" class="m-0 urlProduct-input text-order-input" placeholder="Вставьте ссылку на товар" required>
                      <span id="plusGreen-${count}" class="plus-green-btn"><i class="fas fa-plus"></i></span>
                   </div>
                </div>
@@ -230,7 +238,7 @@
             <div class="order-card" style="min-width: 105px;">
                <div class="mt-2 row">
                   <div>
-                     <input type="number" name="size" class="m-0 text-order-input" required>
+                     <input type="text" name="size" class="m-0 text-order-input" required>
                   </div>
                </div>
             </div>
@@ -255,4 +263,74 @@
       bluePlusBtn.addEventListener('click', blueBtnHandler);
    }
    bluePlusBtn.addEventListener('click', blueBtnHandler);
+
+   const photosUrl = {};
+
+   async function getImageUrl(e) {
+      file = e.target.files[0];
+      const _token = document.querySelector('[name="_token"]').value;
+      const formData = new FormData();
+      formData.append('image', file);
+      formData.append('_token', _token);
+      let res = await fetch('{{Route("UploadOrderPhoto")}}', {
+         method: 'POST',
+         body: formData
+      })
+      res = await res.json();
+      if (photosUrl[e.target.getAttribute('name')]) {
+         photosUrl[e.target.getAttribute('name')] = [...photosUrl[e.target.getAttribute('name')], res.url];
+      } else {
+         photosUrl[e.target.getAttribute('name')] = [res.url];
+      }
+      
+   }
+
+   async function makeOrder() {
+      const dataToServer = [];
+      for (let i = 0; i <= countOfMainInputs; i++) {
+         const data = {},
+               photos = [],
+               urlPhotos = [],
+               urlProducts = [],
+               mainInput = document.getElementById(i),
+               urlPhotoInputs = mainInput.querySelectorAll('[name="ImageUrl"]'),
+               urlProductInputs = mainInput.querySelectorAll('[name="url"]'),
+               count = mainInput.querySelector('[name="count"]').value,
+               cost = mainInput.querySelector('[name="cost"]').value,
+               color = mainInput.querySelector('[name="color"]').value,
+               size = mainInput.querySelector('[name="size"]').value,
+               model = mainInput.querySelector('[name="model"]').value;
+
+         const makeData = (inputs, array, fieldName) => {
+            data[fieldName] = array;
+            inputs.forEach(input => {
+               data[fieldName].push({url: input.value});
+            })
+         }
+         
+         photosUrl[i].forEach(url => {
+            photos.push({url: url});
+         })
+         makeData(urlPhotoInputs, urlPhotos, 'PhotoUrl');
+         makeData(urlProductInputs, urlProducts, 'ProductUrl');
+         data['Photo'] = photos;
+         data['count'] = parseInt(count);
+         data['cost'] = parseInt(cost);
+         data['color'] = color;
+         data['size'] = size;
+         data['model'] = model;
+         dataToServer.push(data);
+      }
+      const res = await fetch('{{Route("CreateOrder")}}', {
+         method: 'POST',
+         headers: {
+            'Content-type': 'application/json',
+            'X-CSRF-TOKEN': document.querySelector('[name="_token"]').value
+         },
+         body: JSON.stringify(dataToServer)
+      })
+      console.log(JSON.stringify(dataToServer));
+      console.log(await res.json());
+   }
+   
 </script>
