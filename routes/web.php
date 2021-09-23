d775e178e938e24e827ae3c5fa5c187c99a16f0a
Troia <vadym.queensoft@gmail.com>
2021-09-23 14:19:24 +0300

    Написал правильный route

--- routes/web.php
+++ routes/web.php
@@ -17,29 +17,38 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return redirect(Route('AuthPage'));
 });
-Route::get('/client', 'PageController@Auth')->middleware('CheckMyAuth')->name('AuthPage');
-Route::get('/client/Recovery', 'PageController@Recovery')->middleware('CheckMyAuth')->name('RecoveryPage');
-Route::get('/client/{page}', 'PageController@page')->middleware('MyAuth')->name('page');
-Route::get('/client/{page}/{subpage}', 'PageController@page')->middleware('MyAuth')->name('Page');
-/* Page route end 
-User route start */
-Route::post('/api/auth', 'UserController@Auth')->middleware('CheckMyAuth')->name('Auth');
-Route::post('/api/register', 'UserController@Register')->middleware('CheckMyAuth')->name('Register');
-Route::post('/api/code', 'UserController@CheckCode')->middleware('CheckMyAuth')->name('CheckCode'); 
-Route::post('/api/recovery', 'UserController@Recovery')->middleware('CheckMyAuth')->name('Recovery');
-Route::post('/api/recovery/last', 'UserController@RecoveryLast')->middleware('CheckMyAuth')->name('RecoveryLast');
-Route::get('/api/exit', 'UserController@Exit')->middleware('MyAuth')->name('Exit');
-/* User route end
-Order route start */
-Route::post('/api/create/order', 'OrderController@CreateOrder')->middleware('MyAuth')->name('CreateOrder');
-Route::post('/api/create/order/upload/image', 'OrderController@UploadOrderPhoto')->middleware('MyAuth')->name('UploadOrderPhoto');
-Route::post('/api/select/order/filter', 'OrderController@Filter')->middleware('MyAuth')->name('Filter');
-Route::post('/api/copy/order', 'OrderController@Copy')->middleware('MyAuth')->name('CopyOrder');
-/* Order route end
-Notification route start */
-Route::post('/api/history/listen/all', 'NotificationController@AllListen')->middleware('MyAuth')->name('AllListen');
-Route::post('/api/history/select', 'NotificationController@Select')->middleware('MyAuth')->name('Select');
-/* Notification route end
-Personal data route start */
-Route::post('/api/personal/info/add', 'AdresController@CreateAdres')->middleware('MyAuth')->name('CreateAdres');
-/* Personal data route end */
+
+
+Route::middleware('MyAuth')->group(function(){ // Other route
+    /* User route start */
+    Route::get('/api/exit', 'UserController@Exit')->name('Exit');
+    /* User route end
+    Order route start */
+    Route::post('/api/create/order', 'OrderController@CreateOrder')->name('CreateOrder');
+    Route::post('/api/create/order/upload/image', 'OrderController@UploadOrderPhoto')->name('UploadOrderPhoto');
+    Route::post('/api/select/order/filter', 'OrderController@Filter')->name('Filter');
+    Route::post('/api/copy/order', 'OrderController@Copy')->name('CopyOrder');
+    /* Order route end
+    Notification route start */
+    Route::post('/api/history/listen/all', 'NotificationController@AllListen')->name('AllListen');
+    Route::post('/api/history/select', 'NotificationController@Select')->name('Select');
+    /* Notification route end
+    Personal data route start */
+    Route::post('/api/personal/info/add', 'AdresController@CreateAdres')->name('CreateAdres');
+    /* Personal data route end
+    Page route start
+    */
+    Route::get('/client/{page}', 'PageController@page')->name('page');
+    Route::get('/client/{page}/{subpage}', 'PageController@page')->name('Page');
+    /* Page route end */
+});
+
+Route::middleware('CheckMyAuth')->group(function(){ // User Route
+    Route::get('/client', 'PageController@Auth')->middleware('CheckMyAuth')->name('AuthPage');
+    Route::get('/client/Recovery', 'PageController@Recovery')->middleware('CheckMyAuth')->name('RecoveryPage');
+    Route::post('/api/auth', 'UserController@Auth')->middleware('CheckMyAuth')->name('Auth');
+    Route::post('/api/register', 'UserController@Register')->middleware('CheckMyAuth')->name('Register');
+    Route::post('/api/code', 'UserController@CheckCode')->middleware('CheckMyAuth')->name('CheckCode'); 
+    Route::post('/api/recovery', 'UserController@Recovery')->middleware('CheckMyAuth')->name('Recovery');
+    Route::post('/api/recovery/last', 'UserController@RecoveryLast')->middleware('CheckMyAuth')->name('RecoveryLast');
+});
