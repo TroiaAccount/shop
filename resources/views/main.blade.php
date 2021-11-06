7e06bdbc1438e4df23c55c4f42d36200faeacc65
Troia <vadym.queensoft@gmail.com>
2021-11-06 16:01:15 +0200

    123

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -23,7 +23,7 @@
             <div class="container body__wrapper">
                <div class="card">
                   <div class="card-body">
-                     @if($subpage == null)
+                     @if($subpage == null || $page == "blank")
                         @include('page/' . $page)
                      @else
                         @include('page/' . $page . '/' . $subpage)
